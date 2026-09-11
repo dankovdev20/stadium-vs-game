9f5c20b1968e0f8ce694c6cb23f6b72b4a0ee9eb
swaagzz <chernenkodanyildev@gmail.com>
2026-09-11 16:33:07 +0200

    init: client (vite+react+tailwind+r3f), tailwind configured

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">tailwind works</h1>
+    </div>
+  );
+}
+
+export default App;
