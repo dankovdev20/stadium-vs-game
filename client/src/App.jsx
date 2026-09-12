79e8f9027ac0036ca5e645f6228872d8afffdc71
Daniil <danil@MacBook-Air-Daniil.local>
2026-09-12 12:28:02 +0200

    added test button for testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
+import Button from './components/common/Button';
+
 function App() {
   return (
     <div className="min-h-screen bg-zinc-900 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-white">tailwind works</h1>
-      
+      <Button>Click me</Button>
     </div>
   );
 }
