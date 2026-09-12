528699c34720acfcbbe93bdfa21bed7339bd0eaa
Daniil <danil@MacBook-Air-Daniil.local>
2026-09-12 11:43:45 +0200

    added new folders for working with components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="min-h-screen bg-zinc-900 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-white">tailwind works</h1>
+      
     </div>
   );
 }
