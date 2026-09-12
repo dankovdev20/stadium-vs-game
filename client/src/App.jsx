29f6a33ee3f78da3a80856e936cdaeb3d75ea04b
swaagzz <chernenkodanyildev@gmail.com>
2026-09-12 12:37:35 +0200

    test: merge conflict test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
+import "./index.css";
+
 function App() {
   return (
     <div className="min-h-screen bg-zinc-900 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-white">tailwind works</h1>
-      
     </div>
   );
 }
