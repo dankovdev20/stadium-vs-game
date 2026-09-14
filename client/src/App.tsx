1e129c1483ada0ffff13dfcbdc9014f2dcb120ae
Daniil <danil@MacBook-Air-Daniil.local>
2026-09-14 15:02:16 +0200

    feat: Added title screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
+import StartScreen from "./screens/StartScreen/StartScreen";
+
 export default function App() {
   return (
     <>
       <div className="bg-zinc-900 text-white min-h-screen flex items-center justify-center">
-        <p className="text-2xl font-bold">tailwind works</p>
+        <StartScreen
+        />
       </div>
     </>
   );
 }
+   
